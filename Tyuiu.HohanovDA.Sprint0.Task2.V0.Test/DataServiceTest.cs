b7d4ce87371f68bc655bd0177ced84cc57d8310c
Хоханов Данил111 <ebefur6@gmail.com>
2025-12-11 07:51:11 +0500

    Task2

---
+++ Tyuiu.HohanovDA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.HohanovDA.Sprint0.Task2.V0.Lib
+namespace Tyuiu.HohanovDA.Sprint0.Task2.V0.Test
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Данил";
+            var res = DataService.GetMessage(name);
+
+            //Вызовем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Данил",res);
+    }
+}
+}
---
+++ Tyuiu.HohanovDA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.HohanovDA.Sprint0.Task2.V0.Lib;
+
+Console.WriteLine(DataService.GetMessage("Данил"));
+Console.ReadKey();
