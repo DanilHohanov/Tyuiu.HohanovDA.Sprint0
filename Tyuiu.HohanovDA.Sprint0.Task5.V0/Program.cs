98234611208934fbc10faecbd02460f508f3333a
Хоханов Данил111 <ebefur6@gmail.com>
2025-12-11 10:25:50 +0500

    Task5 1

--- Tyuiu.HohanovDA.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.HohanovDA.Sprint0.Task5.V0/Program.cs
@@ -9,7 +9,7 @@ class Program
         Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
         Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
         // Пример разветвляющейся структуры находится в библиотеке классов в методе Division
-        Console.WriteLine("A / B = " + DataService.Division(9, 3));
+        Console.WriteLine("A / B = " + DataService.Division(7, 0));
         Console.ReadKey();
     }
 }
