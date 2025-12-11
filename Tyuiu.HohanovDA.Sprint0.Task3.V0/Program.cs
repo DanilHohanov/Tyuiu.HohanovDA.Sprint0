b59175523bee16a2cbf5fe6120330fc130de5ce1
Хоханов Данил111 <ebefur6@gmail.com>
2025-12-11 08:19:45 +0500

    Task3

---
+++ Tyuiu.HohanovDA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿using Tyuiu.HohanovDA.Sprint0.Task3.V0.Lib;
+// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Sum(7, 7));
+        Console.ReadKey();
+    }
+}
